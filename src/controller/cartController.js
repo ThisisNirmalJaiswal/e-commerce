ee26fc71e3d7f750a50fed8a72817be8fe1c7001
sabiha <105931254+ThisisNirmalJaiswal@users.noreply.github.com>
2022-10-18 11:25:12 +0530

    new decoration

--- src/controller/cartController.js
+++ src/controller/cartController.js
@@ -1,7 +1,112 @@
 //--------------------------------------Requiring Cart model from models-------------------------------------
 const cartModel = require("../models/cartModel");
 
-//----------------------------------get cart details---------------------------------
+
+//==========================================create cart API===========================================
+
+const createCart = async function (req, res) {
+  try {
+
+    let requestBody = req.body;
+
+        userIdFromParam = req.params.userId
+
+        let { cartId, productId } = requestBody;
+
+        const productById = await productModel.findById(productId).lean()
+        if (!productById) {
+            return res.status(404).send({ status: false, message: " product not found!!!" })
+        }
+
+        if (productById.isDeleted == true) {
+            return res.status(404).send({ status: false, message: " product is deleted!!!" })
+        }
+
+        if (productById.installments === 0) {
+            return res
+                .status(400)
+                .send({ status: false, message: "product is out of stock" });
+        }
+        const userCart = await cartModel.findOne({ userId: userIdFromParam })
+        //ifcart does not exist for the use
+        if (!userCart) {
+            let filter = {}
+
+            let prodData = { productId: productById._id, quantity: 1 }
+
+            filter.totalItems = 1
+            filter.totalPrice = productById.price
+            filter.userId = userIdFromParam
+            filter.items = prodData
+            const createdCart = await cartModel.create(filter)
+
+            let productDataAll = await cartModel.findOne({ userId: userIdFromParam }).populate('items.productId')
+
+            return res.status(200)
+                .send({ status: true, message: "New cart created with products", data: productDataAll, });
+
+        }
+        if (userCart._id != cartId) {
+            return res
+                .status(400)
+                .send({ status: false, message: "cart doesnot exist" });
+        }
+        //if usercart is created but is empty
+        if (userCart.items.length === 0) {
+            const addedProduct = {
+                productId: productById,
+                quantity: { $inc: +1 }
+            }
+
+            const newItemInCart = await cartModel.findOneAndUpdate({ userId: userIdFromParam }, { $set: { items: [addedProduct] } }, { $inc: { totalItems: +1, totalPrice: +productById.price } }, { new: true }).populate('items.productId')
+           
+
+            return res.status(200).send({
+                status: true,
+                message: "Product added to cart",
+                data: newItemInCart,
+            });
+        }
+        //for checking if product exist in cart
+        {
+            let productExistInCart = userCart.items.findIndex(items => items.productId == requestBody.productId);
+
+            console.log(productExistInCart)
+
+            //if provided product does exist in cart
+            if (productExistInCart > -1) {
+
+                const increasedProductQuantity = await cartModel.findOneAndUpdate({ userId: userIdFromParam, "items.productId": productId }, {
+                    $inc: { totalPrice: +productById.price, totalItems: +1, "items.$.quantity": +1 },
+                }, { new: true }).populate('items.productId')
+                
+                return res.status(200)
+                    .send({ status: true, message: "Product quantity and price updated in the cart", data: increasedProductQuantity });
+            }
+            //if provided product does not exist in cart
+            if (productExistInCart == -1) {
+                const updatedProductQuantity = await cartModel.findOneAndUpdate({ userId: userIdFromParam },
+                    { $push: { items: { productId: productId, quantity: 1 } }, $inc: { totalPrice: +productById.price, totalItems: +1 }, }, { new: true }).populate('items.productId')
+
+                
+
+                return res.status(200)
+                    .send({ status: true, message: "product updated to cart", data: updatedProductQuantity });
+            }
+        }
+
+    }
+    catch (err) {
+        return res.status(500).send({ status: false, message: err.message })
+
+    }
+
+}
+//==========================================create cart API end here===========================================
+
+
+
+//==========================================get cart details API===========================================
 const getCartDetails = async function (req, res) {
   try {
 //--------------------------------------Request UserId from path Params-------------------------------------
